8bbdef59c8d3b9b2e3938df406124bdee0ca58de
apurbar06 <apurbar06@gmail.com>
2023-03-17 18:13:12 +0530

    Declare the states

--- submit-form-data-rest-api/src/App.js
+++ submit-form-data-rest-api/src/App.js
@@ -1,7 +1,13 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
